6198fbce92b0c9dd7f06778c1fac8a7fa60f6e3e
Aleksander Evensen <ahse03@gmail.com>
2022-11-18 00:34:40 +0100

    Finished the jar building script

--- java/MyProgram.java
+++
@@ -1,9 +0,0 @@
-package com.github.aleks.rusty;
-
-public class MyProgram {
-	public static void main(String[] args) {
-		MyProgram.print_string("Hello World");
-	}
-
-	public static native void print_string(String string);
-}
--- jvm/build.rs
+++ jvm/build.rs
@@ -1,27 +1,40 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn main() {
-    println!("cargo:rerun-if-changed=../java/*.java");
-
-    let mut java_child = Command::new("javac")
-        .args(["../java/*.java", "-source", "7", "-target", "7"])
-        .spawn()
+    let java_child = Command::new("javac")
+        .args([
+            "-sourcepath",
+            "../java/",
+            "-d",
+            "../java/out",
+            "../java/com/ahse/jvm/Main.java",
+            "-source",
+            "7",
+            "-target",
+            "7",
+        ])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+        .unwrap();
+    let jar_child = Command::new("jar")
+        .current_dir("../java/out")
+        .args(["cfev", "../jvm-test.jar", "com.ahse.jvm.Main", "*"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
         .unwrap();
 
-    java_child.wait().unwrap();
-
-    let mut jar_child = Command::new("jar")
-        .args(["cvf", "../java/MyProgram.jar", "../java/*.class"])
-        .spawn()
-        .unwrap();
-
-    jar_child.wait().unwrap();
+    let java_output = String::from_utf8(java_child.stdout).unwrap();
+    let java_error = String::from_utf8(java_child.stderr).unwrap();
+    let jar_output = String::from_utf8(jar_child.stdout).unwrap();
+    let jar_error = String::from_utf8(jar_child.stderr).unwrap();
 
     std::fs::write(
         "../java-build-output.txt",
         format!(
-            "===javac===\nSTDOUT:\n{:?}\n\nSTDERR:\n{:?}\n\n\n===jar===\nSTDOUT:\n{:?}\n\nSTDERR:\n{:?}",
-            java_child.stdout.take(), java_child.stderr.take(), jar_child.stdout.take(), jar_child.stderr.take()
+            "===javac===\nSTDOUT:\n\n{}\n\nSTDERR:\n\n{}\n\n\n===jar===\nSTDOUT:\n\n{}\n\nSTDERR:\n\n{}",
+            java_output, java_error, jar_output, jar_error
         )
         .as_bytes(),
     )
