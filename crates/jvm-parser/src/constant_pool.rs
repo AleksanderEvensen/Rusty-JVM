48d625ce002202d7d8dfd556e2384871a2028995
Aleksander Evensen <ahse03@gmail.com>
2023-01-10 21:24:31 +0100

    format: Added Cargo Format

--- crates/jvm-parser/src/constant_pool.rs
+++ crates/jvm-parser/src/constant_pool.rs
@@ -3,16 +3,16 @@ use std::io::Result;
 
 #[derive(Debug, Clone)]
 pub struct ConstantPool {
-    pub pool_entries: Vec<CpInfo>,
+	pub pool_entries: Vec<CpInfo>,
 }
 
 impl ConstantPool {
-    pub fn from_reader(reader: &mut BinaryReader) -> Result<Self> {
-        let pool_count = *reader.read::<u16>()? - 1;
+	pub fn from_reader(reader: &mut BinaryReader) -> Result<Self> {
+		let pool_count = *reader.read::<u16>()? - 1;
 
-        let mut last_was_8byte = false;
+		let mut last_was_8byte = false;
 
-        let entries: Vec<CpInfo> = (0..pool_count)
+		let entries: Vec<CpInfo> = (0..pool_count)
             .map(|i| {
 				if last_was_8byte {
 					last_was_8byte = false;
@@ -47,142 +47,142 @@ impl ConstantPool {
 				};
 			}).collect();
 
-        Ok(Self {
-            pool_entries: entries,
-        })
-    }
+		Ok(Self {
+			pool_entries: entries,
+		})
+	}
 
-    pub fn get_at(&self, index: u16) -> Option<&CpInfo> {
-        self.pool_entries.get(index as usize - 1)
-    }
+	pub fn get_at(&self, index: u16) -> Option<&CpInfo> {
+		self.pool_entries.get(index as usize - 1)
+	}
 
-    pub fn get_class_at(&self, index: u16) -> Option<&CpInfoClass> {
-        if let CpInfo::Class(class) = self.get_at(index).unwrap() {
-            return Some(class);
-        }
-        None
-    }
+	pub fn get_class_at(&self, index: u16) -> Option<&CpInfoClass> {
+		if let CpInfo::Class(class) = self.get_at(index).unwrap() {
+			return Some(class);
+		}
+		None
+	}
 
-    pub fn get_refs_at(&self, index: u16) -> Option<&CpInfoRefs> {
-        if let CpInfo::Refs(refs) = self.get_at(index).unwrap() {
-            return Some(refs);
-        }
-        None
-    }
+	pub fn get_refs_at(&self, index: u16) -> Option<&CpInfoRefs> {
+		if let CpInfo::Refs(refs) = self.get_at(index).unwrap() {
+			return Some(refs);
+		}
+		None
+	}
 
-    pub fn get_name_type_at(&self, index: u16) -> Option<&CpInfoNameAndType> {
-        if let CpInfo::NameAndType(name_type) = self.get_at(index).unwrap() {
-            return Some(name_type);
-        }
-        None
-    }
+	pub fn get_name_type_at(&self, index: u16) -> Option<&CpInfoNameAndType> {
+		if let CpInfo::NameAndType(name_type) = self.get_at(index).unwrap() {
+			return Some(name_type);
+		}
+		None
+	}
 
-    pub fn get_utf8_at(&self, index: u16) -> Option<&CpInfoUtf8> {
-        if let CpInfo::Utf8(utf8) = self.get_at(index).unwrap() {
-            return Some(utf8);
-        }
-        None
-    }
+	pub fn get_utf8_at(&self, index: u16) -> Option<&CpInfoUtf8> {
+		if let CpInfo::Utf8(utf8) = self.get_at(index).unwrap() {
+			return Some(utf8);
+		}
+		None
+	}
 
-    pub fn get_string_at(&self, index: u16) -> Option<&CpInfoString> {
-        if let CpInfo::String(str) = self.get_at(index).unwrap() {
-            return Some(str);
-        }
-        None
-    }
+	pub fn get_string_at(&self, index: u16) -> Option<&CpInfoString> {
+		if let CpInfo::String(str) = self.get_at(index).unwrap() {
+			return Some(str);
+		}
+		None
+	}
 }
 
 // From: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.4
 #[derive(Debug, Clone)]
 pub enum CpInfo {
-    /// This dumb mf was needed because Oracle thought it was funny that 8byte Constant Pool entries shuld take up two spots
-    /// Thankfully they admitted that this was a dumb mistake on their side (too late to fix apparently)
-    /// Source: Check the italic text: https://docs.oracle.com/javase/specs/jvms/se19/html/jvms-4.html#jvms-4.4.5
-    EmptyCpEntry,
+	/// This dumb mf was needed because Oracle thought it was funny that 8byte Constant Pool entries shuld take up two spots
+	/// Thankfully they admitted that this was a dumb mistake on their side (too late to fix apparently)
+	/// Source: Check the italic text: https://docs.oracle.com/javase/specs/jvms/se19/html/jvms-4.html#jvms-4.4.5
+	EmptyCpEntry,
 
-    Class(CpInfoClass),
-    Refs(CpInfoRefs),
-    NameAndType(CpInfoNameAndType),
-    Utf8(CpInfoUtf8),
-    String(CpInfoString),
-    Integer(CpInfoInteger),
-    Float(CpInfoFloat),
-    Long(CpInfoLong),
-    Double(CpInfoDouble),
-    InvokeDynamic(CpInfoInvokeDynamic),
-    MethodHandle(CpInfoMethodHandle),
-    MethodType(CpInfoMethodType),
+	Class(CpInfoClass),
+	Refs(CpInfoRefs),
+	NameAndType(CpInfoNameAndType),
+	Utf8(CpInfoUtf8),
+	String(CpInfoString),
+	Integer(CpInfoInteger),
+	Float(CpInfoFloat),
+	Long(CpInfoLong),
+	Double(CpInfoDouble),
+	InvokeDynamic(CpInfoInvokeDynamic),
+	MethodHandle(CpInfoMethodHandle),
+	MethodType(CpInfoMethodType),
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoInteger {
-    pub tag: &'static str,
-    pub bytes: i32,
+	pub tag: &'static str,
+	pub bytes: i32,
 }
 #[derive(Debug, Clone)]
 pub struct CpInfoFloat {
-    pub tag: &'static str,
-    pub bytes: f32,
+	pub tag: &'static str,
+	pub bytes: f32,
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoLong {
-    pub tag: &'static str,
-    pub bytes: u64,
+	pub tag: &'static str,
+	pub bytes: u64,
 }
 #[derive(Debug, Clone)]
 pub struct CpInfoDouble {
-    pub tag: &'static str,
-    pub bytes: f64,
+	pub tag: &'static str,
+	pub bytes: f64,
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoClass {
-    pub tag: &'static str,
-    pub name_index: u16,
+	pub tag: &'static str,
+	pub name_index: u16,
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoRefs {
-    pub tag: &'static str,
-    pub class_index: u16,
-    pub name_and_type_index: u16,
+	pub tag: &'static str,
+	pub class_index: u16,
+	pub name_and_type_index: u16,
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoNameAndType {
-    pub tag: &'static str,
-    pub name_index: u16,
-    pub descriptor_index: u16,
+	pub tag: &'static str,
+	pub name_index: u16,
+	pub descriptor_index: u16,
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoUtf8 {
-    pub tag: &'static str,
-    pub data: String,
+	pub tag: &'static str,
+	pub data: String,
 }
 #[derive(Debug, Clone)]
 pub struct CpInfoString {
-    pub tag: &'static str,
-    pub string_index: u16,
+	pub tag: &'static str,
+	pub string_index: u16,
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoInvokeDynamic {
-    pub tag: &'static str,
-    pub bootstrap_method_attr_index: u16,
-    pub name_and_type_index: u16,
+	pub tag: &'static str,
+	pub bootstrap_method_attr_index: u16,
+	pub name_and_type_index: u16,
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoMethodHandle {
-    pub tag: &'static str,
-    pub reference_kind: u8,
-    pub reference_index: u16,
+	pub tag: &'static str,
+	pub reference_kind: u8,
+	pub reference_index: u16,
 }
 
 #[derive(Debug, Clone)]
 pub struct CpInfoMethodType {
-    pub tag: &'static str,
-    pub descriptor_index: u16,
+	pub tag: &'static str,
+	pub descriptor_index: u16,
 }
--- jvm/src/main.rs
+++ jvm/src/main.rs
@@ -22,54 +22,54 @@ macro_rules! dbgprint {
 #[macro_export]
 #[cfg(not(feature = "debug"))]
 macro_rules! dbgprint {
-    () => {};
-    ($($arg:tt)*) => {};
+	() => {};
+	($($arg:tt)*) => {};
 }
 
 #[derive(Parser, Debug)]
 #[command(author,version,about,long_about = None)]
 struct Args {
-    /// The path to the .jar or .class file to be executed
-    path: Option<PathBuf>,
+	/// The path to the .jar or .class file to be executed
+	path: Option<PathBuf>,
 
-    /// Toggles the debug prints
-    #[arg(short, long)]
-    debug: bool,
+	/// Toggles the debug prints
+	#[arg(short, long)]
+	debug: bool,
 }
 
 fn main() {
-    // let args = Args::parse();
+	// let args = Args::parse();
 
-    // let file_path = args
-    //     .path
-    //     .or_else(|| Some("./java/MyProgram.class".into()))
-    //     .unwrap();
+	// let file_path = args
+	//     .path
+	//     .or_else(|| Some("./java/MyProgram.class".into()))
+	//     .unwrap();
 
-    // let class_file = ClassFile::from_file(&file_path).unwrap();
+	// let class_file = ClassFile::from_file(&file_path).unwrap();
 
-    let jar_file = JarFile::from_file(&PathBuf::from("./java/jvm-test.jar")).unwrap();
+	let jar_file = JarFile::from_file(&PathBuf::from("./java/jvm-test.jar")).unwrap();
 
-    println!("Jar File: \n{:#?}", jar_file);
+	println!("Jar File: \n{:#?}", jar_file);
 
-    // dbgprint!(
-    //     "Magic: {:X?}\nVersion: {} : {}",
-    //     &class_file.magic,
-    //     &class_file.major_version,
-    //     &class_file.minor_version
-    // );
-    // #[cfg(feature = "debug")]
-    // {
-    //     class_file
-    //         .constant_pool
-    //         .pool_entries
-    //         .iter()
-    //         .enumerate()
-    //         .for_each(|(i, cp_info)| dbgprint!("[{}] cp_info = {:?}", i + 1, cp_info));
-    // }
+	// dbgprint!(
+	//     "Magic: {:X?}\nVersion: {} : {}",
+	//     &class_file.magic,
+	//     &class_file.major_version,
+	//     &class_file.minor_version
+	// );
+	// #[cfg(feature = "debug")]
+	// {
+	//     class_file
+	//         .constant_pool
+	//         .pool_entries
+	//         .iter()
+	//         .enumerate()
+	//         .for_each(|(i, cp_info)| dbgprint!("[{}] cp_info = {:?}", i + 1, cp_info));
+	// }
 
-    // let mut jvm = JVM::new();
-    // jvm.add_class_file(class_file).add_class_file(class_file_2);
-    // jvm.run();
+	// let mut jvm = JVM::new();
+	// jvm.add_class_file(class_file).add_class_file(class_file_2);
+	// jvm.run();
 
-    // println!("{:#?}", class_file);
+	// println!("{:#?}", class_file);
 }
