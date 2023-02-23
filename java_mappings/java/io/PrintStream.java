b07d40c5a0900c85fdb1df054427012bc65ce9da
Aleksander Evensen <ahse03@gmail.com>
2023-02-23 17:58:47 +0100

    Removed some unneeded java code

--- java_mappings/java/io/PrintStream.java
+++
@@ -1,5 +0,0 @@
-package java_mappings.java.io;
-
-public class PrintStream {
-	public native void println(String str);
-}
--- java_mappings/java/lang/System.java
+++
@@ -1,9 +0,0 @@
-package java_mappings.java.lang;
-import java_mappings.java.io.PrintStream;
-
-public class System {
-
-	public static PrintStream out = null;
-
-
-}
